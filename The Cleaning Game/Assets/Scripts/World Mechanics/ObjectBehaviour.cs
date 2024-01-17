82e561241c437abb7c9627f5790b5e195acf9267
EZBreezy98 <104875936+EZBreezy98@users.noreply.github.com>
2024-01-17 19:40:39 +1100

    RoomManager
    Room manager now adds items to a list of objects and can remove them when the object leaves the room and still add it back again later. It also can mark an object as deviant

---
+++ The Cleaning Game/Assets/Scripts/World Mechanics/ObjectBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBehaviour : MonoBehaviour
+{
+    public bool isDeviant;
+
+    bool isDirty;
+    bool isDeviantDirty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CommitDeviantAct()
+    {
+
+    }
+}
